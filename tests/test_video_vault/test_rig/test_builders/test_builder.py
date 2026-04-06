c79a20fc7d982990394e6eafd4aa178db05d681a
Winipedia <winipedia@gmx.de>
2026-04-06 23:02:30 +0100

    test fix

--- tests/test_video_vault/test_rig/test_builders/test_builder.py
+++ tests/test_video_vault/test_rig/test_builders/test_builder.py
@@ -4,6 +4,9 @@
 class TestVideoVaultBuilder:
     """Test class."""
 
+    def test_app_icon_png_location(self) -> None:
+        """Test method."""
+
     def test_entry_point_module(self) -> None:
         """Test method."""
 
