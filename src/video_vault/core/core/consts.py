c86ba429f8e1e756f8492fa2bdddb8f68bd4c810
Winipedia <winipedia@gmx.de>
2026-04-15 16:44:24 +0100

    pyrig update

--- src/video_vault/core/core/consts.py
+++ src/video_vault/core/core/consts.py
@@ -1,6 +1,6 @@
 """This module contains constants for the application."""
 
-from pyrig.core.string_ import make_name_from_obj
+from pyrig.core.strings import make_name_from_obj
 
 import video_vault
 
