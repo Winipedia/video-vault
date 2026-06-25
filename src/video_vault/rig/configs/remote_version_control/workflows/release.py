5f0eda1a1be60d0fcdc4ac3a17dc8dc80136fca6
Winipedia <winipedia@gmx.de>
2026-06-25 11:30:58 +0100

    pyrig upgrade

--- src/video_vault/rig/configs/remote_version_control/workflows/release.py
+++ src/video_vault/rig/configs/remote_version_control/workflows/release.py
@@ -6,7 +6,7 @@ All subclasses of ConfigFile in the configs package are automatically called.
 from collections.abc import Iterable
 from types import ModuleType
 
-from pyrig_executables.rig.configs.remote_version_control.workflows.release import (  # deptry: ignore[DEP004]  # noqa: E501
+from pyrig_executables.rig.configs.version_control.remote.workflows.release import (  # deptry: ignore[DEP004]  # noqa: E501
     ReleaseWorkflowConfigFile as ExecutablesReleaseWorkflowConfigFile,
 )
 from winipyside.rig import resources
