8001ee804cf81f696f021315471b84027c09169a
Winipedia <winipedia@gmx.de>
2026-07-18 02:17:16 +0100

    pyrig fixtures integration of a test to check if init works with the plugin

--- src/video_vault/rig/tools/tools.py
+++ src/video_vault/rig/tools/tools.py
@@ -1,19 +1,7 @@
 """Override pyrig tools."""
 
 from winidjango.rig.tools.tools import ProjectTester as BaseProjectTester
-from winidjango.rig.tools.tools import Pyrigger as BasePyrigger
 from winipyside.rig.tools.project_tester import ProjectTester as BaseProjectTester2
-
-
-class Pyrigger(BasePyrigger):
-    """Pyrigger class for video_vault."""
-
-    def dev_dependencies(self) -> tuple[str, ...]:
-        """Get the dev dependencies."""
-        return (
-            *super().dev_dependencies(),
-            "yt-dlp-types",
-        )
 
 
 class ProjectTester(BaseProjectTester, BaseProjectTester2):
