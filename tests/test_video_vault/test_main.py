fea22c276632bba62b7ba697c0ff436af2989c46
Winipedia <winipedia@gmx.de>
2026-07-18 02:54:32 +0100

    simplified

--- tests/test_video_vault/test_main.py
+++ tests/test_video_vault/test_main.py
@@ -1,11 +1,9 @@
 """test module."""
 
-from pathlib import Path
-
 
 def test_main() -> None:
     """Test func for main."""
 
 
-def test_run(tmp_project_root_path: Path, tmp_source_root_path: Path) -> None:
+def test_run() -> None:
     """Test func for main."""
